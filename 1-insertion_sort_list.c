eedd7d20ba13b2144378af411c94f06f50632ef7
effgrandad <effgrandad@gmail.com>
2024-01-23 10:59:25 -0800

    commit all the changes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -37,7 +37,7 @@ void insertion_sort_list(listint_t **list)
 	{
 		temp = iter->next;
 		insert = iter->prev;
-		while (insert != NULL && iter->m < insert->m)
+		while (insert != NULL && iter->n < insert->n)
 		{
 			swap_nodes(list, &insert, iter);
 			print_list((const listint_t *)*list);
